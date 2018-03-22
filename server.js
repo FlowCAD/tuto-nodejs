e2a7c30525e54702702bc13a4ffcad60fcb060c6
FlowCAD <florian.cadoz@gmail.com>
2018-03-22 20:01:16 +0100

    Request in url parsed

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 let http = require('http')
 let fs = require('fs')
+let url = require('url')
 
+const headerParam = {'Content-type': 'text/html; charset=utf-8'}
 let server = http.createServer()
 server.on('request', (request, response) => {
-    fs.readFile('index.html', (err, data) => {
+    response.writeHead(200, headerParam)
+    let query = url.parse(request.url, true).query
+    let name = query.name === undefined ? 'anonyme' : query.name
+    fs.readFile('index.html', 'utf-8', (err, data) => {
         if(err) {
-            response.writeHead(404)
+            response.writeHead(404, headerParam)
             response.end('404 not found !!')
         } else {
-            response.writeHead(200, {
-                'Content-type': 'text/html; charset=utf-8'
-          })
+            response.writeHead(200, headerParam)
+            data = data.replace('{{ name }}', name)
+            response.end(data)
         }
-        response.end(data)
     })
 })
 
