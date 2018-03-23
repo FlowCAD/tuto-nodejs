be7cb5a754acc86c77786aab38300661ced443c0
FlowCAD <florian.cadoz@gmail.com>
2018-03-23 17:36:49 +0100

    Pipe used for writing big files

--- 03-streams/copy_big_file.js
+++ 03-streams/copy_big_file.js
@@ -5,12 +5,17 @@ fs.stat(file, (err, stat) => {
     let total = stat.size
     let progress = 0
     let read = fs.createReadStream(file)
+    let write = fs.WriteStream('copy.mp4')
     read.on('data', (chunk) => {
         progress += chunk.length
         console.log(`Lecture à ${Math.round(100 * progress / total)} %`)
     })
-    
+
+    read.pipe(write)
     read.on('end', () => {
         console.log('Fin de lecture')
+    })
+    write.on('finish', () => {
+        console.log('Fichier copié !')
     })
 })
