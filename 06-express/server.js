b6873cff8f541aca5a54629f154b03c146f2683a
FlowCAD <florian.cadoz@gmail.com>
2018-03-26 00:38:02 +0200

    Express-session configured

--- 06-express/server.js
+++ 06-express/server.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
+let session = require('express-session')
 
 
 // Template
@@ -11,16 +12,27 @@ app.set('view engine', 'ejs')
 app.use('/assets', express.static('public'))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use(session({
+    secret: 'My awesome secret key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}))
 
 
 // Routes
 app.get('/', (req, res) => {
+    if (req.session.error) {
+        res.locals.error = req.session.error
+        req.session.error = undefined
+    }
     res.render('pages/index')
 })
 
 app.post('/', (req, res) => {
     if(!req.body.message) {
-        res.render('pages/index', {error: "Message vide"})
+        req.session.error = "Il y a une erreur"
+        res.redirect('/')
     }
     console.log(req.body)
 })
