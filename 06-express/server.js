095b4d6ad268d2951f54f33365a30c84f467a75c
FlowCAD <florian.cadoz@gmail.com>
2018-03-25 23:35:40 +0200

    Empty message managed

--- 06-express/server.js
+++ 06-express/server.js
@@ -1,12 +1,29 @@
 let express = require('express')
 let app = express()
+let bodyParser = require('body-parser')
 
+
+// Template
 app.set('view engine', 'ejs')
 
-app.use('/assets', express.static('public'))
 
+// Middleware
+app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+// Routes
 app.get('/', (req, res) => {
-    res.render('pages/index', {test: 'Coucou !'})
+    res.render('pages/index')
 })
+
+app.post('/', (req, res) => {
+    if(!req.body.message) {
+        res.render('pages/index', {error: "Message vide"})
+    }
+    console.log(req.body)
+})
+
 
 app.listen(8080)
